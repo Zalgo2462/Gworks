41fe7035721b07eec473a5b2b9ff071a4902bee5
Wade <zalgo2462@gmail.com>
2012-12-31 01:50:07 -0700

    Changed framerate class a bit

--- GWorks Test/src/org/zp/gtest/rendertests/Framerate.java
+++ GWorks Test/src/org/zp/gtest/rendertests/Framerate.java
@@ -8,26 +8,29 @@ import java.awt.*;
 
 public class Framerate implements GPaintStrategy {
 	private final GLoop loop;
+	private long currentFrame = 0;
 	private double min = Double.MAX_VALUE;
 	private double max = Double.MIN_VALUE;
-	private double deviation = Double.MAX_VALUE;
 
 	public Framerate(GCanvas canvas) {
 		this.loop = canvas.getLoop();
 	}
 	@Override
 	public void paint(GCanvas canvas, Graphics graphics) {
+		currentFrame++;
 		graphics.setColor(Color.WHITE);
 		double framerate = loop.getActualFramerate();
 		graphics.drawString(String.valueOf(framerate) + " FPS", 25, 25);
-		if(framerate < min && framerate != -1.0D)
-			min = framerate;
-		if(framerate > max)
-			max = framerate;
-		deviation = max - min;
-		graphics.drawString("Min: " + String.valueOf(min) + " FPS", 25, 45);
-		graphics.drawString("Max: " + String.valueOf(max) + " FPS", 25, 65);
-		graphics.drawString("Dev: " + String.valueOf(deviation) + " FPS", 25, 85);
+		if(currentFrame > canvas.FPS) {
+			if(framerate < min && framerate != -1.0D)
+				min = framerate;
+			if(framerate > max)
+				max = framerate;
+			final double deviation = max - min;
 
+			graphics.drawString("Min: " + String.valueOf(min) + " FPS", 25, 45);
+			graphics.drawString("Max: " + String.valueOf(max) + " FPS", 25, 65);
+			graphics.drawString("Dev: " + String.valueOf(deviation) + " FPS", 25, 85);
+		}
 	}
 }
