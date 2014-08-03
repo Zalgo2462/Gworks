e50732c7ab891cb051116fb396cfd84132652d81
Wade <zalgo2462@gmail.com>
2014-08-03 04:06:40 -0600

    catching up github

--- BlockDude/src/org/zp/blockdude/Sprites/game/Missile.java
+++ BlockDude/src/org/zp/blockdude/Sprites/game/Missile.java
@@ -10,15 +10,20 @@ import java.awt.image.BufferedImage;
  * Time: 12:20 AM
  */
 public class Missile extends Sprite {
+	private final long MAX_AGE = 1250000000L;
 	private final Color color;
 	private BufferedImage image;
+	private long age;
+	private double maxDamage;
 
-	public Missile(final Color color) {
+	public Missile(final Color color, final double maxDamage) {
 		this.color = color;
+		this.age = 0;
+		this.maxDamage = maxDamage;
 		createSprite();
 		renderer.setSprite(image);
 		movement.setAcceleration(200);
-		movement.setMaxSpeed(300);
+		movement.setMaxSpeed(325);
 		movement.setDeceleration(-200);
 	}
 
@@ -28,5 +33,18 @@ public class Missile extends Sprite {
 		g.setColor(color);
 		g.fillRect(0, 0, 20, 5);
 		g.dispose();
+	}
+
+	public double getDamage() {
+		return (maxDamage * age / MAX_AGE);
+	}
+
+	public void age(long delta) {
+		age += delta;
+		renderer.setOpacity(1 - (1F * age / MAX_AGE));
+	}
+
+	public boolean checkDecay() {
+		return age > MAX_AGE;
 	}
 }
